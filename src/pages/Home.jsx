bc49c913f6e1b5b0aaf23dca526a3e27438058cb
Arnab Chatterjee <arnabchatterjee912@gmail.com>
2024-03-05 22:15:16 +0530

    hero

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,14 +8,14 @@ const Home = () => {
         <Typography
           sx={{
             fontWeight: 500,
-            fontSize: "3rem",
+            fontSize: "2.6rem",
           }}
         >
           Welcome to <span style={{ color: "#EB4132" }}>Google </span>
-          <span style={{ color: "#4086F4" }}>Developer </span> <br />
+          <span style={{ color: "#4086F4" }}>Developer </span> <br/>
           <span style={{ color: "#069E56" }}>Students </span>
-          <span style={{ color: "#FABC00" }}>Club </span> of{" "}
-          <span className="code">Techno India University</span>
+          <span style={{ color: "#FABC00" }}>Club </span>of{" "}
+          <span>Techno India University</span>
         </Typography>
         <Typography
           sx={{
@@ -23,14 +23,17 @@ const Home = () => {
             mt: 2,
           }}
         >
-          Helping students bridge the gap between theory and practice" Connect.
-          Learn. Grow Developer Student Clubs (DSC) is a program in
-          collaboration with Google Developers that targets passionate students
-          from developer communities across the globe. Be it Android, Cloud, or
-          Data Science - this is a unique platform for students to not just
-          learn through P2P workshops but solve actual real-world problems for
-          the local community through sponsored projects. JIT Coimbatore is now
-          part of the few institutes that have been selected for this program.
+          Building Bridges Between Theory and Practice: Engage. Empower.
+          Elevate. Techno India University's Google Developer Student Clubs
+          (GDSC) is a dynamic initiative in partnership with Google Developers,
+          aimed at nurturing the passion of budding developers within our
+          university community. Whether delving into Web, Android, Cloud
+          Computing, or Data Science and many more, our platform offers an
+          exclusive space for students to not only participate in peer-to-peer
+          workshops but also to tackle real-world challenges through sponsored
+          projects, making meaningful contributions to our local community. Join
+          us as we proudly become a beacon of innovation among the select
+          institutions chosen for this prestigious program.
         </Typography>
       </Box>
     </Box>

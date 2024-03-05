137dae4efc98acc45875815964504adaf857012e
Arnab Chatterjee <arnabchatterjee912@gmail.com>
2024-03-05 14:28:54 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import LOGO from '/gdsc.png'
+
+function App() {
+
+  return (
+    <div className='app'>
+      <img src={LOGO} alt="gdsc logo" className='logo'/>
+      <h1>GOOGLE DEVELOPER STUDENTS CLUB</h1>
+      <h2>TECHNO INDIA UNIVERSITY</h2>
+    </div>
+  )
+}
+
+export default App
